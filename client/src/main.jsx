c03aad0e86dfd63acdbcc69d470981fa42e48c10
Richard Nyul <nyulricsi@gmail.com>
2023-12-10 17:35:22 +0100

    added navigation for homepage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -5,6 +6,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
