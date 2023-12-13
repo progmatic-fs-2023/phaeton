3307920d6adb1334c38b0c63385877e09bea6c18
Richard Nyul <nyulricsi@gmail.com>
2023-12-13 20:36:42 +0100

    Navigation for header component created and removed unnecessary files

---
+++ client/src/components/HeaderNav.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import './styles/HeaderNav.css';
+
+function HeaderNav() {
+  return (
+    <div className="header-nav-container">
+      <NavLink className="header-nav-elem" to="/rental">
+        Rent
+      </NavLink>
+      <NavLink className="header-nav-elem" to="/parking">
+        Parking
+      </NavLink>
+      <NavLink className="header-nav-elem" to="/shuttle">
+        Shuttle
+      </NavLink>
+      <NavLink className="header-nav-elem" to="/contact">
+        Contact
+      </NavLink>
+    </div>
+  );
+}
+
+export default HeaderNav;
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import './Login.css';
+import './styles/Login.css';
 
 function Login() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
