2cedf5285897c6f73f1b2d3484001f300f6f12d8
Richard Nyul <nyulricsi@gmail.com>
2023-12-16 09:43:26 +0100

    Minor changes according to the review and moving the record create functions into a distinct file

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(express.json());
 
-
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
--- server/prisma/migrations/DBdata.js
+++ server/prisma/migrations/DBdata.js
@@ -4,51 +4,47 @@ const prisma = new PrismaClient();
 
 // Run these functions once to fill the database
 async function createLot(idx) {
-    const newParkingLot = await prisma.parkingLot.create({
-      data: {
-        name: `A${idx}`,
-      },
-    })
-    console.log(newParkingLot)
-
-  }
+  const newParkingLot = await prisma.parkingLot.create({
+    data: {
+      name: `A${idx}`,
+    },
+  });
+  console.log(newParkingLot);
+}
 
 for (let i = 1; i <= 15; i += 1) {
-    createLot(i)
-
+  createLot(i);
 }
 
 async function CreateAdmin() {
-    const user = await prisma.Users.create({
-      data: {
-        firstName: `John`,
-        lastName: 'Doe',
-        email: 'johndoe@email.com',
-        password: 'password123',
-        role: 'ADMIN',
-        driverLicence: 'AS123456'
-      },
-    })
-    console.log(user)
-
-  }
+  const user = await prisma.Users.create({
+    data: {
+      firstName: `John`,
+      lastName: 'Doe',
+      email: 'johndoe@email.com',
+      password: 'password123',
+      role: 'ADMIN',
+      driverLicence: 'AS123456',
+    },
+  });
+  console.log(user);
+}
 //   CreateAdmin
 
 async function CreateCar() {
-    const car = await prisma.Cars.create({
-      data: {
-        model: 'Ford Fiesta',
-        imageUrl: 'ford_fiesta.jpg',
-        seats: 5,
-        transmission: 'M',
-        price: 12000,
-        fuel: 'Petrol',
-        power: 56,
-        trunkCapacity: 2,
-      },
-    })
-    console.log(car)
-
-  }
+  const car = await prisma.Cars.create({
+    data: {
+      model: 'Ford Fiesta',
+      imageUrl: 'ford_fiesta.jpg',
+      seats: 5,
+      transmission: 'M',
+      price: 12000,
+      fuel: 'Petrol',
+      power: 56,
+      trunkCapacity: 2,
+    },
+  });
+  console.log(car);
+}
 
 //   CreateCar()
