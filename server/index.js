effc3a0166c2a90994601b28874566f42ed34ca8
Purplefingerex <acsaia@hotmail.com>
2023-12-18 18:08:24 +0100

    user routes created, login and sign up authentication created

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import 'dotenv/config';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const port = process.env.PORT;
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
+
+app.get('/', async (req, res) => {
+  const result = await prisma.Users.findMany();
+  console.log(result);
+  res.json(result);
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import logger from './middlewares/logger.middleware';
-import errorHandler from './middlewares/errorHandler.middleware';
-import apiRouter from './routes/api.route';
+import logger from './middlewares/logger.middleware.js';
+import errorHandler from './middlewares/errorHandler.middleware.js';
+import apiRouter from './routes/api.route.js';
+import usersRouter from './routes/users.routes.js';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 
 app.use('/api', apiRouter);
+
+app.use('/users', usersRouter);
 
 app.use(errorHandler);
 export default app;
