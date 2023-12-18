c911e1da9bb6c9cb607c1d57a8793059f741c1df
Purplefingerex <acsaia@hotmail.com>
2023-12-18 18:08:24 +0100

    user routes created, login and sign up authentication created

---
+++ server/src/routes/users.routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import * as authController from '../controllers/auth.controller.js';
+
+const router = express.Router();
+
+router.get('/:id/profile');
+router.post('/signup', authController.signUp);
+router.post('/login', authController.login);
+export default router;
---
+++ server/src/services/users.service.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function createUser(newUser) {
+  const user = await prisma.users.create({
+    data: newUser,
+  });
+  return user;
+}
+
+export async function loginUser(userInput) {
+  const user = await prisma.users.findUnique({
+    where: userInput,
+  });
+  return user;
+}
+
+export async function findUserByEmail(email) {
+  const user = await prisma.users.findUnique({
+    where: {
+      email,
+    },
+  });
+  return user;
+}
