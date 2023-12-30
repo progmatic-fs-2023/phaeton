2d0c39a3b76be8dc9e2a49e31c92196b59a2e53e
Alexander Acsai <acsaia@hotmail.com>
2023-12-30 14:15:45 +0100

    ParkingZone and Map component created, styles also added

--- client/src/pages/Parking.jsx
+++ client/src/pages/Parking.jsx
@@ -1,11 +1,12 @@
 import { useCallback, useState } from 'react';
 import DatePicker from '../components/DatePicker';
 import BackGroundContext from '../contexts/BackgroundContext';
+import ParkingZones from '../components/ParkingZones';
+import '../components/styles/Parking.css';
 
 function Parking() {
   const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null)
-
+  const [endDate, setEndDate] = useState(null);
 
   function getStartDate(data) {
     setStartDate(data);
@@ -15,31 +16,37 @@ function Parking() {
     setEndDate(data);
   }
 
-  const handleGetStartDate = useCallback((data) => {
-    getStartDate(data);
-  }, [getStartDate]);
-  
-  const handleGetEndDate = useCallback((data) => {
-    getEndDate(data);
-  }, [getEndDate]);
-
-  if(!startDate && !endDate) {
-  return (
-    <div>
-      <BackGroundContext.Provider value="component-background parking-bg">
-        <DatePicker getStartDate={handleGetStartDate} getEndDate={handleGetEndDate} />
-      </BackGroundContext.Provider>
-    </div>
+  const handleGetStartDate = useCallback(
+    (data) => {
+      getStartDate(data);
+    },
+    [getStartDate],
   );
-  }
+
+  const handleGetEndDate = useCallback(
+    (data) => {
+      getEndDate(data);
+    },
+    [getEndDate],
+  );
+
+  if (!startDate && !endDate) {
     return (
       <div>
-        <BackGroundContext.Provider value="opened">
+        <BackGroundContext.Provider value="component-background parking-bg">
           <DatePicker getStartDate={handleGetStartDate} getEndDate={handleGetEndDate} />
-          <p>Parking</p>
         </BackGroundContext.Provider>
       </div>
-    )
+    );
+  }
+  return (
+    <div>
+      <BackGroundContext.Provider value="opened">
+        <DatePicker getStartDate={handleGetStartDate} getEndDate={handleGetEndDate} />
+      </BackGroundContext.Provider>
+      <ParkingZones />
+    </div>
+  );
 }
 
 export default Parking;
