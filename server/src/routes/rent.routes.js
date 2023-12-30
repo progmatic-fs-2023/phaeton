d9a77f849719560e9236de4eaf9642b8debbb8bf
Richard Nyul <nyulricsi@gmail.com>
2023-12-30 21:09:13 +0100

    backend date problem solved

--- server/src/routes/rent.routes.js
+++ server/src/routes/rent.routes.js
@@ -5,7 +5,7 @@ import * as userIdValidator from '../middlewares/userIdValidator.middleware';
 const router = express.Router();
 
 router.get('/', rentController.list);
-router.get('/date', rentController.listByDate);
+router.post('/date', rentController.listByDate);
 router.get('/:id', rentController.listById);
 router.patch('/date/:id', userIdValidator.userIdValidatorInBody, rentController.rent);
 
