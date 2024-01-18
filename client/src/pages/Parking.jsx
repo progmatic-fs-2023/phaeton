4d3b43c44f6f6f717da4e8b2d1941c98e2070123
Purplefingerex <acsaia@hotmail.com>
2024-01-18 20:48:33 +0100

    finaly error removed

--- client/src/pages/Parking.jsx
+++ client/src/pages/Parking.jsx
@@ -19,19 +19,22 @@ function Parking() {
       const formattedEndDate = formatDate(endDate);
       navigate(`/parking/from/${formattedStartDate}/end/${formattedEndDate}`);
     }
-  }, [startDate, endDate, navigate]);
+  };
 
-  if (!startDate && !endDate) {
-    return (
-      <div>
-        <h1 className="page-title mobile">Parking</h1>
-        <h1 className="page-title desktop">Parking - Secure your Car While You&apos;re Away</h1>
-        <BackGroundContext.Provider value="component-background parking-bg">
-          <DatePicker getStartDate={handleGetStartDate} getEndDate={handleGetEndDate} />
-        </BackGroundContext.Provider>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1 className="page-title mobile">Parking</h1>
+      <h1 className="page-title desktop">Parking - Secure your Car While You&apos;re Away</h1>
+      <BackGroundContext.Provider value="component-background parking-bg">
+        <h1>Parking Page</h1>
+        <DatePicker
+          onSearch={(startDate, endDate) => {
+            onSearchFn(startDate, endDate);
+          }}
+        />
+      </BackGroundContext.Provider>
+    </div>
+  );
 }
 
 export default Parking;
