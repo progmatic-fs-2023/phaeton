015e045613f96ab1eb8f28839bdf3b4cf020f81d
Ádám Székely <adam@szekely.xyz>
2024-01-20 09:51:46 +0100

    Fix lint errors

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -10,8 +10,8 @@ import Shuttle from '../pages/Shuttle';
 import Profile from '../pages/ProfilePage';
 import Booking from '../pages/Booking';
 import ParkingZonesPage from '../pages/Parking/ParkingZonesPage';
-import RentalPage from '../pages/Rent/RentalPage';
 import ServiceForm from '../pages/ServiceForm';
+import ParkingDetailsContext from '../contexts/ParkingDetailsContext';
 
 function Layout() {
   const [parkingData, setParkingData] = useState(null);
