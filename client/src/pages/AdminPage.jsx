dba05ff9689dbe9d1af640b0577e415c6fdb15ec
Richard Nyul <nyulricsi@gmail.com>
2024-01-24 18:37:01 +0100

    loadingscreen and filter added

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,12 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
+import LoadingScreen from '../components/ReusableComponents/LoadingScreen';
 import UserContext from '../contexts/UserContext';
 import '../components/styles/Pages/AdminPage.css';
 
 function AdminPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [originalServices, setOriginalServices] = useState(null);
   const [services, setServices] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [activeFilterIndex, setActiveFilterIndex] = useState(null);
 
   const userCtx = useContext(UserContext);
 
@@ -22,6 +26,8 @@ function AdminPage() {
       throw new Error('Network response was not ok');
     }
     setServices(data.result);
+    setOriginalServices(data.result);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -40,9 +46,25 @@ function AdminPage() {
     }
   };
 
-  // const handleSelectChange = (event) => {
-  //   // majd valami
-  // };
+  function handleFilterButton(event, index) {
+    let filteredData;
+    if (event.target.innerHTML === 'Rents') {
+      filteredData = originalServices.filter((service) => service.Cars);
+    } else if (event.target.innerHTML === 'Parkings') {
+      filteredData = originalServices.filter((service) => service.ParkingLot);
+    } else {
+      filteredData = originalServices;
+    }
+    setActiveFilterIndex(index);
+    setServices(filteredData);
+    setCurrentPage(1);
+  }
+
+  const handleSelectChange = (event, index) => {
+    if (event.target.value !== 'active') {
+      console.log(index);
+    }
+  };
 
   function isDisabled(service) {
     let boolean = false;
@@ -78,98 +100,114 @@ function AdminPage() {
   }
 
   const navigate = useNavigate();
+  if (!isLoading) {
+    if (services) {
+      if (userCtx.user.role === 'ADMIN') {
+        const indexOfLastItem = currentPage * itemsPerPage;
+        const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+        const currentItems = services.slice(indexOfFirstItem, indexOfLastItem);
 
-  if (services) {
-    if (userCtx.user.role === 'ADMIN') {
-      const indexOfLastItem = currentPage * itemsPerPage;
-      const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-      const currentItems = services.slice(indexOfFirstItem, indexOfLastItem);
-
-      return (
-        <div className="admin-page-container">
-          <h1>Welcome {userCtx.user.firstName}!</h1>
-          <label htmlFor="item-per-page">
-            Items per Page
-            <select
-              id="item-per-page"
-              onChange={(event) => {
-                setItemsPerPage(event.target.value);
-              }}
-              name="item-per-page"
-            >
-              <option>10</option>
-              <option>15</option>
-              <option>25</option>
-              <option>50</option>
-            </select>
-          </label>
-          <table className="services-table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Start Date</th>
-                <th>End Date</th>
-                <th>Actual End Date</th>
-                <th>Parking Zone</th>
-                <th>Name</th>
-                <th>Car Model</th>
-                <th>Phone Number</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {currentItems.map((service) => (
-                <tr key={service.id}>
-                  <td>{service.id}</td>
-                  <td>{dateFormatter2(service.ServiceStartDate)}</td>
-                  <td>{dateFormatter2(service.ServiceEndDate)}</td>
-                  <td>{dateFormatter2(service.ActualServiceEndDate)}</td>
-                  <td>{service.ParkingLot ? service.ParkingLot.zone : '-'}</td>
-                  <td>
-                    {service.Users.firstName} {service.Users.lastName}
-                  </td>
-                  <td>{service.Cars ? service.Cars.model : '-'}</td>
-                  <td>
-                    <a href={`tel:${service.PhoneNumber}`}>{service.PhoneNumber}</a>
-                  </td>
-                  <td>
-                    <select
-                      disabled={isDisabled(service)}
-                      className={defaultStatus(service)}
-                      defaultValue={defaultStatus(service)}
-                      // onChange={handleSelectChange}
-                    >
-                      <option className="active" value="active">
-                        Active
-                      </option>
-                      <option className="returned" value="returned">
-                        Returned
-                      </option>
-                      <option className="canceled" value="canceled">
-                        Canceled
-                      </option>
-                    </select>
-                  </td>
-                </tr>
+        return (
+          <div className="admin-page-container">
+            <h1>Welcome {userCtx.user.firstName}!</h1>
+            <label htmlFor="item-per-page">
+              Items per Page
+              <select
+                id="item-per-page"
+                onChange={(event) => {
+                  setItemsPerPage(event.target.value);
+                }}
+                name="item-per-page"
+              >
+                <option>10</option>
+                <option>15</option>
+                <option>25</option>
+                <option>50</option>
+              </select>
+            </label>
+            <div className="admin-page-filter-button">
+              {['All', 'Parkings', 'Rents'].map((filter, index) => (
+                <button
+                  key={filter}
+                  type="button"
+                  onClick={(event) => handleFilterButton(event, index)}
+                  className={activeFilterIndex === index ? 'active-filter' : ''}
+                >
+                  {filter}
+                </button>
               ))}
-            </tbody>
-          </table>
-          {currentPage > 1 && (
-            <button type="button" onClick={handlePrevClick}>
-              Previous
-            </button>
-          )}
-          {currentPage < Math.ceil(services.length / itemsPerPage) && (
-            <button type="button" onClick={handleNextClick}>
-              Next
-            </button>
-          )}
-        </div>
-      );
+            </div>
+            <table className="services-table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Start Date</th>
+                  <th>End Date</th>
+                  <th>Actual End Date</th>
+                  <th>Parking Zone</th>
+                  <th>Name</th>
+                  <th>Car Model</th>
+                  <th>Phone Number</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentItems.map((service, index) => (
+                  <tr key={service.id}>
+                    <td>{service.id}</td>
+                    <td>{dateFormatter2(service.ServiceStartDate)}</td>
+                    <td>{dateFormatter2(service.ServiceEndDate)}</td>
+                    <td>{dateFormatter2(service.ActualServiceEndDate)}</td>
+                    <td>{service.ParkingLot ? service.ParkingLot.zone : '-'}</td>
+                    <td>
+                      {service.Users.firstName} {service.Users.lastName}
+                    </td>
+                    <td>{service.Cars ? service.Cars.model : '-'}</td>
+                    <td>
+                      <a href={`tel:${service.PhoneNumber}`}>{service.PhoneNumber}</a>
+                    </td>
+                    <td>
+                      <select
+                        disabled={isDisabled(service)}
+                        className={defaultStatus(service)}
+                        defaultValue={defaultStatus(service)}
+                        onChange={(event) => handleSelectChange(event, index)}
+                      >
+                        <option className="active" value="active">
+                          Active
+                        </option>
+                        <option className="returned" value="returned">
+                          Returned
+                        </option>
+                        <option className="canceled" value="canceled">
+                          Canceled
+                        </option>
+                      </select>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div>
+              {currentPage > 1 && (
+                <button type="button" onClick={handlePrevClick}>
+                  Previous
+                </button>
+              )}
+              {currentPage < Math.ceil(services.length / itemsPerPage) && (
+                <button type="button" onClick={handleNextClick}>
+                  Next
+                </button>
+              )}
+            </div>
+          </div>
+        );
+      }
+      useEffect(() => {
+        navigate('/');
+      }, []);
     }
-    useEffect(() => {
-      navigate('/');
-    }, []);
   }
+  return <LoadingScreen />;
 }
 export default AdminPage;
